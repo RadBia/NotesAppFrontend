b662002b29842b4d9778edf8d8f4d02b1f00ea18
RadBia <rad.bialkowski@gmail.com>
2023-03-25 15:04:01 +0100

    Add user login functionality using React Router and Fetch API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState, useEffect} from 'react';
+import {
+  BrowserRouter as Router,
+  Routes as Switch,
+  Route
+} from "react-router-dom";
+import Login from './components/login/Login';
+
 
 function App() {
+  const [isLogged, setIsLogged] = useState(false);
+  const [id, setid] = useState(localStorage.getItem('id'));
+  const [theme, setTheme] = useState(true)
+  let render;
+
+  useEffect(() => {
+    function checkLoginStatus() {
+      if(id != null) {
+        setIsLogged(true);
+      }
+    }
+
+    function checkTheme() {
+      let boolean_ = localStorage.getItem('theme');
+      let res_ = false;
+      if(boolean_ == "false") {
+        res_ = false;
+      } else {
+        res_ = true;
+      }
+      setTheme(res_);
+    }
+
+    checkTheme();
+    checkLoginStatus();
+  }, []);
+
+  let loggedContent = () => {return (<div className={theme == false ? "wrapper" : "dark wrapper"}>
+                                         <div className="content">
+                                          <Switch>
+                                          </Switch>
+                                        </div>
+                                      </div>)};
+
+  let notLoggedContent = () => {return (<div className="wrapper">
+                                          <Switch>
+                                          </Switch>
+                                        </div>)};
+  if(!isLogged) {
+    render = notLoggedContent();
+  } else {
+    render = loggedContent();
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      { render }
+    </Router>
   );
 }
 
